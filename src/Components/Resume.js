957cc4612ef577cba7807dac183b3bfa43362d34
sam <samxtasnim@gmail.comgit confing --global user.emailgit config --global user.email samxtasnim@gmail.com>
2021-06-19 16:37:00 +0600

    resume updated ver-4

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Resume = () => {
   return (
     <div className="container">
-      <iframe src="https://drive.google.com/file/d/1y02E353ITKZU4MiZ0SrHkceoglDybNoB/preview" title="resume" width="100%" height="800px"></iframe>
+      <iframe src="https://drive.google.com/file/d/1WUU4C6KMfyM0b4Kzl9Y9KQSa7-SztQn_/preview" title="resume" width="100%" height="800px"></iframe>
     </div>
   );
 };
